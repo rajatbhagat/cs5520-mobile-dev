6be6dbc48ab29704fb74c68360efca07276bb36f
rajatbhagat <rajat.bhagat@gmail.com>
2022-06-11 16:21:54 -0400

    Adding start and terminate search buttons

--- app/src/main/java/com/cs5520/mobile_dev/FindPrimeActivity.java
+++ app/src/main/java/com/cs5520/mobile_dev/FindPrimeActivity.java
@@ -8,6 +8,8 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class FindPrimeActivity extends AppCompatActivity {
@@ -17,21 +19,42 @@ public class FindPrimeActivity extends AppCompatActivity {
     private TextView currentNumTextView;
     private TextView latestPrimeTextView;
     private Handler textHandler = new Handler();
+    private Button findPrimeButton;
+    private Button terminateSearchButton;
+    private boolean endSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_prime);
         currentNumTextView = (TextView) findViewById(R.id.current_num_text);
-        currentNumTextView.setText("2");
         latestPrimeTextView = (TextView) findViewById(R.id.latest_prime_text);
 
-        ChildThread childThread = new ChildThread();
-        new Thread(childThread).start();
+        findPrimeButton = (Button) findViewById(R.id.start_search_button);
+        terminateSearchButton = (Button) findViewById(R.id.terminate_search_button);
+
+        findPrimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endSearch = false;
+                currentNumTextView.setText("2");
+                latestPrimeTextView.setText("");
+                ChildThread childThread = new ChildThread();
+                new Thread(childThread).start();
+            }
+        });
+
+        terminateSearchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endSearch = true;
+            }
+        });
     }
 
     @Override
     public void onBackPressed() {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage("Do you want to terminate the search?");
         builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
@@ -40,7 +63,7 @@ public class FindPrimeActivity extends AppCompatActivity {
                 finish();
             }
         });
-        builder.setNegativeButton("No",new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 dialog.cancel();
@@ -54,40 +77,46 @@ public class FindPrimeActivity extends AppCompatActivity {
     private boolean isPrime(int num) {
         if (num == 0 || num == 1) {
             return false;
-        } else {
-            int div = 2;
-            while ( div != num) {
-                if (num % div == 0) {
-                    return false;
-                }
-                div++;
-            }
+        }
+        if (num == 2) {
             return true;
         }
+        if (num % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     protected class ChildThread implements Runnable {
-
+        int originalNum = 2;
         @Override
         public void run() {
-            int originalNum = 2;
-            while (true) {
+            while (!endSearch) {
                 boolean ans = isPrime(originalNum);
                 if (ans) {
-                    int finalOriginalNum = originalNum;
                     textHandler.post(new Runnable() {
                         @SuppressLint("SetTextI18n")
                         @Override
                         public void run() {
-                            latestPrimeTextView.setText("" + finalOriginalNum);
+                            latestPrimeTextView.setText(Integer.toString(originalNum));
                         }
                     });
-                    Log.d(TAG, "Running on a different thread using Runnable Interface: " + finalOriginalNum);
                 }
                 originalNum++;
                 try {
-                    Thread.sleep(1000);
-                    currentNumTextView.setText(originalNum);
+                    textHandler.post(new Runnable() {
+                        @SuppressLint("SetTextI18n")
+                        @Override
+                        public void run() {
+                            currentNumTextView.setText(Integer.toString(originalNum));
+                        }
+                    });
+                    Thread.sleep(100);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
