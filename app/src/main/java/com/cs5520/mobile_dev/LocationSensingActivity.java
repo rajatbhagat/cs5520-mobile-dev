09dca5b7cf44ad3a7ba36ad95609589dedd6768a
rajatbhagat <rajat.bhagat@gmail.com>
2022-06-18 12:55:38 -0400

    Adding the Location Activity and button on the main activity

---
+++ app/src/main/java/com/cs5520/mobile_dev/LocationSensingActivity.java
@@ -0,0 +1,14 @@
+package com.cs5520.mobile_dev;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LocationSensingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location_sensing);
+    }
+}
