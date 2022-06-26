7910cc47ed920f6374217d2216451a9f4bad1b4c
rajatbhagat <rajat.bhagat@gmail.com>
2022-06-26 13:30:34 -0400

    Adding a child thread instead of AsyncTask

--- app/src/main/java/com/cs5520/mobile_dev/ApiSearch.java
+++ app/src/main/java/com/cs5520/mobile_dev/ApiSearch.java
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.annotation.SuppressLint;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -39,6 +40,7 @@ public class ApiSearch extends AppCompatActivity {
     private TextView displayTextView;
     private RecyclerView recyclerView;
     List<AnimeData> animeDataList = new ArrayList<>();
+    private Handler listHandler = new Handler();
     private AnimeSearchAdapter animeSearchAdapter;
 
     @Override
@@ -69,10 +71,94 @@ public class ApiSearch extends AppCompatActivity {
     }
 
     public void callWebserviceButtonHandler(View view){
-        PingWebServiceTask task = new PingWebServiceTask();
-//         Insert search params in the execute
-        task.execute(); // This is a security risk.  Don't let your user enter the URL in a real app.
+//        PingWebServiceTask task = new PingWebServiceTask();
+////         Insert search params in the execute
+//        task.execute(); // This is a security risk.  Don't let your user enter the URL in a real app.
 
+        ApiSearch.ChildThread childThread = new ApiSearch.ChildThread();
+        new Thread(childThread).start();
+
+    }
+
+    protected class ChildThread implements Runnable {
+        int originalNum = 2;
+
+        @Override
+        public void run() {
+            /**
+             * Sample URL to get data: https://api.jikan.moe/v4/anime?q=naruto&type=movie&start_date=2015
+             *
+             * Search params:
+             * name,
+             * type: tv, movie, special
+             * status: airing, complete, upcoming
+             * start_date: (only year)
+             */
+
+
+
+            URL url = null;
+            try {
+//                url = new URL("https://api.jikan.moe/v4/anime?q=naruto&type=movie&start_date=2015");
+                url = new URL("https://api.jikan.moe/v4/anime?q=naruto");
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+
+                conn.connect();
+
+                // Read response.
+                InputStream inputStream = conn.getInputStream();
+                final String resp = convertStreamToString(inputStream);
+                JSONObject jObject = new JSONObject(resp);
+                JSONArray jsonArray = jObject.getJSONArray("data");    // Use this if your web service returns an array of objects.  Arrays are in [ ] brackets.
+                for (int i = 0; i< jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    AnimeData data = new AnimeData();
+                    data.setAnimeName(jsonObject.get("title").toString());
+//                    data.setAnimeScore(Integer.parseInt(jsonObject.get("score").toString()));
+                    data.setAnimeStatus(jsonObject.get("status").toString());
+                    data.setAnimeType(jsonObject.get("type").toString());
+                    data.setAnimeSynopsis(jsonObject.get("synopsis").toString());
+
+                    JSONObject imageObject = jsonObject.getJSONObject("images").getJSONObject("webp");
+                    data.setAnimeImageURL(imageObject.get("image_url").toString());
+//                    data.setYoutubeTrailerURL(jsonObject.getJSONObject("trailer"));
+//
+                    animeDataList.add(data);
+                }
+
+                listHandler.post(new Runnable() {
+                    @SuppressLint("SetTextI18n")
+                    @Override
+                    public void run() {
+                       animeSearchAdapter.updateList(animeDataList);
+                    }
+                });
+
+            } catch (MalformedURLException e) {
+                Log.e(TAG,"MalformedURLException");
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                Log.e(TAG,"ProtocolException");
+                e.printStackTrace();
+            } catch (IOException e) {
+                Log.e(TAG,"IOException");
+                e.printStackTrace();
+            } catch (JSONException e) {
+                Log.e(TAG,"JSONException");
+                e.printStackTrace();
+            }
+
+//            return animeDataList;
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+//        }
+        }
     }
 
     @SuppressLint("StaticFieldLeak")
